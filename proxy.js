efe00ab7e79915244d121e6eb0eef903266f4ff9
Eugene Prokopiev <enp@itx.ru>
2020-05-27 10:07:52 +0300

    use level as block

--- proxy.js
+++ proxy.js
@@ -1,37 +1,21 @@
 const http = require('http')
 
 const handler = (req, res) => {
-    let block, date
-    const end = (block, date) => {
-        if (block && date) {
-            date = Math.round((new Date() - new Date(date))/1000)
-            res.write('HELP Block\n')
-            res.write('TYPE block gauge\n')
-            res.write(`block ${block}\n`)
-            res.write('HELP Time\n')
-            res.write('TYPE time gauge\n')
-            res.write(`time ${date}\n`)
-            res.end()
-        }
-    }
-    http.get('http://node:8732/chains/main/blocks', (subres) => {
-        let data = ''
-        subres.on('data', (chunk) => { data += chunk })
-        subres.on('end', () => {
-            console.log(data)
-            data = JSON.parse(data)
-            block = data[0][0]
-            end(block, date)
-        })
-    }).end()
     http.get('http://node:8732/chains/main/blocks/head/header', (subres) => {
         let data = ''
         subres.on('data', (chunk) => { data += chunk })
         subres.on('end', () => {
             console.log(data)
             data = JSON.parse(data)
-            date = data.timestamp
-            end(block, date)
+            const block = data.level
+            const date = Math.round((new Date() - new Date(data.timestamp))/1000)
+            res.write('HELP Block\n')
+            res.write('TYPE block counter\n')
+            res.write(`block ${block}\n`)
+            res.write('HELP Time\n')
+            res.write('TYPE time gauge\n')
+            res.write(`time ${date}\n`)
+            res.end()
         })
     }).end()
 }
